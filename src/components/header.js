5e12ecb597ff54ed7f0f702c4a84212110b82792
andre <andre_w3@yahoo.com>
2019-04-18 16:10:03 -0700

    menu fixed working on font

--- src/components/header.js
+++ src/components/header.js
@@ -14,8 +14,8 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: `960`,
-        padding: `1.45rem 1.0875rem`,
+        maxWidth: `100%`,
+        // padding: `1.45rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
