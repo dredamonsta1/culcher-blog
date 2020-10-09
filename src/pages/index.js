9d1390ac51f653059dd8c56e004be03c7b7e94d3
andre <andre_w3@yahoo.com>
2020-10-09 00:32:27 -0400

    adding smoothe scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 import {graphql, StaticQuery } from 'gatsby'
-import Post from '../components/Post'
+// import Post from '../components/Post'
 
 
 // import { Button } from 'reactstrap';
@@ -17,7 +17,11 @@ const IndexPage = () => (
   
   
     <SEO title="Home" keywords={[`HipHop`, `application`, `react`]} />
-    
+    <div className='main' id='top'>
+      <a href="#bottom">
+        bottom
+      </a>
+    </div>
     <h1>Welcome to the 9X4 </h1>
     <p>Listing the coulture</p>
     
@@ -29,6 +33,11 @@ const IndexPage = () => (
     }}/>
     <Link to="/">Go back to the homepage</Link>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    </div>
+    <div className='main' id='bottom'>
+      <a href="#top">
+        top
+      </a>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
     <Link to="Blog/">Blog</Link>
