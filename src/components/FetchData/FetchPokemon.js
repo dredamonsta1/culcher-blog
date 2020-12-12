bac07449b6a1fbe7b52cf70b0c050cb24d5b4c56
andre <andre_w3@yahoo.com>
2020-12-11 19:09:14 -0500

    started new component to fetch api

---
+++ src/components/FetchData/FetchPokemon.js
@@ -0,0 +1,4 @@
+const pokemon = ('https://pokeapi.co/api/v2/pokemon/{id or name}/')
+
+fetch(pokemon);
+console.log(pokemon);
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/layout';
-
-
-
-
+import FetchPokemon from '../components/FetchData/FetchPokemon';
 
 
 
 const Projects = () => {
+    // const [poke, setPoke] = useState();
+    
+    console.log(FetchPokemon);
     return (
         <Layout>
         <h1>Hello Projects</h1>
+
+
+        <div>
+            <h6>pokemon here</h6>
+        </div>
+        
+
+
         </Layout>
+
         )
 }
 
